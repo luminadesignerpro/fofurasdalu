9cf1e13d50b5026598a6009001c369773b53792d
SD Moveis Dev <dev@sdmoveis.com>
2026-05-25 11:39:23 -0300

    Fix login URL and redesign admin page

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 // supabase.js
-const SUPABASE_URL = 'https://grzjokafhdewxvyvkydza.supabase.co';
+const SUPABASE_URL = 'https://grzjokafhdewxvykydza.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_sM_rR8jaU_yaYnuZCm1vdA_iA0rJsYk';
 
 // Inicializa o cliente do Supabase (usando nome diferente para evitar conflito)
